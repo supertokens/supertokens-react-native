b1d145729fbae2a5a308270b9ff417fab2812ad8
Rishabh Poddar <rishabh.poddar@gmail.com>
2020-03-28 22:05:08 +0530

    changing implementation approach

--- ios/Pods/Target Support Files/SuperTokensSession/SuperTokensSession-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SuperTokensSession : NSObject
-@end
-@implementation PodsDummy_SuperTokensSession
-@end
--- ios/RNSuperTokens.m
+++ ios/RNSuperTokens.m
@@ -1,3 +1,17 @@
+/* Copyright (c) 2020, VRAI Labs and/or its affiliates. All rights reserved.
+*
+* This software is licensed under the Apache License, Version 2.0 (the
+* "License") as published by the Apache Software Foundation.
+*
+* You may not use this file except in compliance with the License. You may
+* obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+* WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+* License for the specific language governing permissions and limitations
+* under the License.
+*/
 
 #import "RNSuperTokens.h"
 #import "SuperTokensSession-Swift.h"
@@ -21,7 +35,7 @@ RCT_EXPORT_METHOD(initLib: (NSString *)refreshTokenEndpoint:
     [SuperTokens initialiseWithRefreshTokenEndpoint:refreshTokenEndpoint sessionExpiryStatusCode:sessionExpiryStatusCode refreshAPICustomHeaders:refreshAPICustomHeaders error:&error];
     
     if (error) {
-        reject(@"no_events", @"There were no events", error);
+        reject(@"init_failed", @"Call to initialising native SDK failed", error);
     } else {
         resolve(nil);
     }
@@ -33,6 +47,22 @@ RCT_EXPORT_METHOD(doesSessionExist:
 {
     NSNumber *result = [SuperTokens doesSessionExist] ? [NSNumber numberWithBool:YES] : [NSNumber numberWithBool:NO];
     resolve(result);
+}
+
+RCT_EXPORT_METHOD(dataTask:
+                  (NSURLRequest *)request:
+                  (RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+    [SuperTokensURLSession
+     dataTaskWithRequest:request
+     completionHandler:^(NSData * data, NSURLResponse * response, NSError * error) {
+        if (error) {
+            reject(@"network_failed", @"Network request failed", error);
+        } else {
+            
+        }
+    }];
 }
 
 @end
