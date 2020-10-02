0de148408712ddf96993263cf51e72b58afb28b4
luizosoares <luizotavio@ioasys.com.br>
2020-10-02 16:17:13 -0300

    refactor: update imports to use community async storage

--- lib/ts/antiCsrf.ts
+++ lib/ts/antiCsrf.ts
@@ -13,7 +13,7 @@
  * under the License.
  */
 
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-community/async-storage";
 
 // TODO: if native is linked, do not use in memory values - always make a call to native
 // This is because there is a chance that native side has changed the anti-csrf token, and here, we are still using the older one.
