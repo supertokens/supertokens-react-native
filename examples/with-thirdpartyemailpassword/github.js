e362a42e74f30387c5021967251eef4833fec107
Nemi Shah <nemishah1212@gmail.com>
2021-11-09 16:30:19 +0530

    Adds example app for social auth (#34)
    * Add example app for social login
    
    * Add readme for example app
    
    * Add example app for social auth
    
    * Update sample app to use multiple apple providers
    
    * Add comments and refactor based on PR review
    
    * Add comments and refactor based on PR review
    
    * Refactor
    
    * Use supertokens-node branch 8.1

---
+++ examples/with-thirdpartyemailpassword/github.js
@@ -0,0 +1,65 @@
+import { authorize } from 'react-native-app-auth';
+import SuperTokens from "supertokens-react-native";
+import { API_DOMAIN } from "./App";
+import axios from 'axios';
+
+// Adds request and response interceptors to the axios instance (Not required if using fetch)
+SuperTokens.addAxiosInterceptors(axios);
+
+/*
+    We use react-native-app-auth to login with Github and then use the response to sign in with SuperTokens
+    When we use authorize to communicate with Google, their servers respond with something similar to:
+
+    {
+        "accessToken": null,
+        "additionalParameters": {},
+        "authorizationCode": "<authorizationCode>",
+        "codeVerifier": "<codeVerifier>",
+        "idToken": null,
+        "scopes": [],
+        "tokenType": null,
+    }
+
+    We send the authorizationCode to our backend /signinup API. The API uses the code to fetch the access_token and
+    user information, and logs the user into SuperTokens.
+*/
+const loginWithGithub = async () => {
+    const clientId = "8a9152860ce869b64c44";
+    // Full configuration options here: https://github.com/FormidableLabs/react-native-app-auth#config
+    const config = {
+        redirectUrl: 'com.demoapp://oauthredirect',
+        clientId: clientId,
+        scopes: ["read:user", "user:email"],
+        additionalHeaders: { 'Accept': 'application/json' }, // For Github this is required
+        serviceConfiguration: {
+            authorizationEndpoint: 'https://github.com/login/oauth/authorize',
+            tokenEndpoint: 'https://github.com/login/oauth/access_token',
+            revocationEndpoint:
+                'https://github.com/settings/connections/applications/' + clientId
+        },
+        skipCodeExchange: true, // This allows us to retrieve the authorization code from Github and request for access token ourselves using SuperTokens
+    };
+
+    // Uses react-native-app-auth to authenticate with Github
+    let authResult = await authorize(config);
+
+    /*
+      We use the authorizationCode returned by the Github servers to sign in the user and create a session using SuperTokens.
+     */
+    let signInUpResponse = await axios.post(`${API_DOMAIN}/auth/signinup`, {
+        redirectURI: "com.demoapp://oauthredirect",
+        thirdPartyId: "github",
+        code: authResult.authorizationCode,
+        clientId: "8a9152860ce869b64c44",
+    }, {
+        headers: {
+            rid: "thirdpartyemailpassword",
+        }
+    });
+
+    if (signInUpResponse.data.status !== "OK") {
+        throw new Error("Github login failed")
+    }
+}
+
+export default loginWithGithub;
---
+++ examples/with-thirdpartyemailpassword/google.js
@@ -0,0 +1,66 @@
+import { authorize } from 'react-native-app-auth';
+import SuperTokens from "supertokens-react-native";
+import { API_DOMAIN } from "./App";
+
+import axios from 'axios';
+
+// Adds request and response interceptors to the axios instance (Not required if using fetch)
+SuperTokens.addAxiosInterceptors(axios);
+
+/*
+    We use react-native-app-auth to login with Google and then use the response to sign in with SuperTokens.
+    When we use authorize to communicate with Google, their servers respond with something similar to:
+    {
+        "accessToken": "<access_token>",
+        "accessTokenExpirationDate": "2021-11-03T13:27:37Z",
+        "authorizeAdditionalParameters": {...},
+        "idToken": "<id_token>",
+        "refreshToken": "<refresh_token",
+        "scopes": [
+            "email",
+            "https://www.googleapis.com/auth/userinfo.email",
+            "openid"
+        ],
+        "tokenAdditionalParameters": {},
+        "tokenType": "Bearer"
+    }
+
+    We send this object to our backend /signinup API which will use the access_token to get the user's information
+    and log the user into SuperTokens.
+*/
+const loginWithGoogle = async () => {
+    // Full configuration options here: https://github.com/FormidableLabs/react-native-app-auth#config
+    const config = {
+        issuer: 'https://accounts.google.com',
+        clientId: '1060725074195-c7mgk8p0h27c4428prfuo3lg7ould5o7.apps.googleusercontent.com',
+        redirectUrl: 'com.demoapp:/oauthredirect',
+        scopes: ["https://www.googleapis.com/auth/userinfo.email"]
+    };
+
+    // This will authenticate with Google and give us the access token to use with SuperTokens
+    let authResult = await authorize(config);
+
+    // react-native-app-auth returns `accessToken` instead of `access_token`, we change this to follow the RFC.
+    authResult.access_token = authResult.accessToken;
+    delete authResult.accessToken;
+
+    /*
+      We use the object returned by the Google servers to sign in the user and create a session using SuperTokens.
+     */
+    let signInUpResponse = await axios.post(`${API_DOMAIN}/auth/signinup`, {
+        redirectURI: "com.demoapp:/oauthredirect",
+        thirdPartyId: "google",
+        authCodeResponse: authResult,
+        clientId: "1060725074195-c7mgk8p0h27c4428prfuo3lg7ould5o7.apps.googleusercontent.com"
+    }, {
+        headers: {
+            rid: "thirdpartyemailpassword",
+        }
+    });
+
+    if (signInUpResponse.data.status !== "OK") {
+        throw new Error("Google login failed")
+    }
+}
+
+export default loginWithGoogle;
