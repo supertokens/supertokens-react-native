6df4308afdb103cb8b130c70b28ceba068a755e8
Nemi Shah <nemishah1212@gmail.com>
2023-09-13 15:58:11 +0530

    Update FDI version

--- lib/ts/version.ts
+++ lib/ts/version.ts
@@ -12,6 +12,6 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-export const package_version = "4.0.6";
+export const package_version = "4.0.7";
 
-export const supported_fdi = ["1.16", "1.17"];
+export const supported_fdi = ["1.16", "1.17", "1.18"];
