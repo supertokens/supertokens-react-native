84236c137a212b8445f44f20539be1e0d185b020
Mihaly Lengyel <mihaly@lengyel.tech>
2024-11-01 11:21:44 +0100

    ci: try test splitting

--- .circleci/setupAndTestWithFreeCore.sh
+++ .circleci/setupAndTestWithFreeCore.sh
@@ -51,7 +51,7 @@ cd ../../
 npm i -d
 cp -r ./test/tough-cookie ./node_modules/
 npm i git+https://github.com:supertokens/supertokens-react-native.git#dev-v$3
-SUPERTOKENS_CORE_TAG=$coreTag INSTALL_PATH=../../supertokens-root npm test
+SUPERTOKENS_CORE_TAG=$coreTag INSTALL_PATH=../../supertokens-root npm run test-ci
 if [[ $? -ne 0 ]]
 then
     echo "test failed... exiting!"
